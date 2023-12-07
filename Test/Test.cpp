5c5be6f0197a6e48c7b0d6927961d3025db95c16
Niels Hofman <hofmanniel@gmail.com>
2023-12-07 12:06:24 +0100

    Setup memory leak detection

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,16 +1,33 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include "Core.h"
 #include "Rendering.h"
 #pragma warning(disable:26451)
 #include <SDL.h>
 
+namespace
+{
+	void quit(Core* core)
+	{
+		core->Quit();
+	}
+}
+
 int main(int argc, char* argv[])
 {
-	Core core{};
-	Rendering* rendering = core.CreateRenderer();
+	//Have this in separate scope so all destructors are called before dumping memory leaks
+	{
+		Core core{};
+		Rendering* rendering = core.CreateRenderer();
 
-	auto testWindow = rendering->createWindow("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_OPENGL);
+		auto testWindow = rendering->createWindow("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_OPENGL);
 
-	core.Run();
+		core.Run();
+	}
+
+	_CrtDumpMemoryLeaks();
 
 	return 0;
 }
